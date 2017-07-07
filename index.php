192cc0d0a27e028a1bc7129bd030b4c82c6486c4
aleksey <elrodir777@gmail.com>
2017-07-07 18:19:43 +0300

    message

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
     session_start();
 ?>
 
+
 <!DOCTYPE   html>
 
 <html   lang="en">
 
-<head
+<head>
 
     <meta   charset="UTF-8">
 
     <titel>Url Resizer</titel>
 
     <link   rel   ="stylesheet"   href="css/style.css">
+
+</head>
+
+<body>
+
+
+</body>
 </html>
