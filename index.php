ac3c82af57035427154e69f63611079df63ce20c
aleksey <elrodir777@gmail.com>
2017-07-07 18:18:29 +0300

    message

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE   html>
+
+<html   lang="en">
+
+<head
+
+    <meta   charset="UTF-8">
+
+    <titel>Url Resizer</titel>
+
+    <link   rel   ="stylesheet"   href="css/style.css">
+</html>
