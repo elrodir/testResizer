4d997a445891abbdb8f68df20a0927f64cc86e4a
aleksey <elrodir777@gmail.com>
2017-07-07 16:18:36 +0300

    message

--- classes/resizer.php
+++ classes/resizer.php
@@ -2,6 +2,17 @@
 class   Shortener   {
     protected   $db;
 
-    public function __construct()   {
+    public  function    __construct()   {
         $this->db   =   new Mysqli('localhost', 'root', '', 'website');
     }
+
+
+    public  function generateCode($num)  {
+        return  base_convert($num,  10, 36);
+    }
+
+
+    public  function    makeCode($url)  {
+        $url    =   trim($url);
+    }
+}
