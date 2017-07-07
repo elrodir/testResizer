162c8eba1e45be15b84728a7ae4ac94073932f95
aleksey <elrodir777@gmail.com>
2017-07-07 17:35:59 +0300

    message

--- resize.php
+++ resize.php
@@ -8,4 +8,10 @@
     if(isset($_POST['url']))    {
 
         $url    =   $_POST['url'];
+
+        if($code    =   $s->makeCode($url))   {
+
+            $_SESSION['feedback']   =   "Ready! Your    link    :
+                <a  href='$url'>http://resizer/$code</a";
+        }
     }
